4302d2695b8f5fd38b6eda00b8b7958f57c832c4
Juan Alejandro Martinez Linares <alej1286@gmail.com>
2023-01-18 07:58:33 -0500

    aded tasks routes

--- server/routes/tasks.routes.js
+++ server/routes/tasks.routes.js
@@ -1,4 +1,21 @@
 import { Router } from "express";
+import {
+  getTasks,
+  createTask,
+  deleteTask,
+  getTask,
+  updateTask,
+} from "../controllers/tasks.controller.js";
 const router = Router();
+
+router.get("/tasks", getTasks);
+
+router.post("/tasks", createTask);
+
+router.put("/tasks/:id", updateTask);
+
+router.delete("/tasks/:id", deleteTask);
+
+router.get("/tasks:id", getTask);
 
 export default router;
