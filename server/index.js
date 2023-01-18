8e5929b4f3a17111e1a52bc1b9af19d9b63ab83f
Juan Alejandro Martinez Linares <alej1286@gmail.com>
2023-01-17 22:19:20 -0500

    change port to 5000 of server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
-app.use(indexRoutes);
+//app.use(indexRoutes);
 
 app.listen(PORT);
 //console.log("Server listening on port 3000");
