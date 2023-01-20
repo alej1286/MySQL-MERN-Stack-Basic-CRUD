f67b48a182efb4a3237edace64aa2f90c1ea9779
Juan Alejandro Work <alej1286@gmail.com>
2023-01-20 16:01:49 -0500

    Begining Tasks listing

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const getTasksRequest = async () => {
+  return await axios.get("http://localhost:5000/tasks");
+};
+
 export const createTaskRequest = async (task) => {
   return await axios.post(`http://localhost:5000/tasks`, task);
 };
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,7 +1,28 @@
-const TaskPage = () => {
-  return (
-    <div>TaskPage</div>
-  )
-}
+import { useEffect,useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
 
-export default TaskPage
+const TaskPage = () => {
+  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    async function loadTasks() {
+      const response = await getTasksRequest();
+      setTasks(response.data);
+    }
+
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      {tasks.map(task=>(
+        <div key={task.id}>
+          <h2>{task.title}</h2>
+        </div>
+      )
+      )}
+    </div>
+  );
+};
+
+export default TaskPage;
