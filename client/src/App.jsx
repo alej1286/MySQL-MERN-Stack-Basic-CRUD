82478c7f04c848e90872b434f79e2ffc44f883ed
Juan Alejandro Work <alej1286@gmail.com>
2023-02-09 18:58:40 -0500

    Styling NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Taskform from "./pages/Taskform";
 import TaskPage from "./pages/TaskPage";
 import NotFound from "./pages/NotFound";
@@ -6,18 +7,26 @@ import Navbar from "./components/Navbar";
 import { TaskContextProvider } from "./context/TaskProvider";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  }
   return (
-    <div className="bg-zinc-900 h-screen">
-      <Navbar />
+    <div
+      className={`h-full w-full mx-auto py-2 h-screen 
+    ${darkMode ? "dark" : ""}`}
+    >
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <div className="container mx-auto py-4 px-20">
-      <TaskContextProvider>
-      <Routes>
-        <Route path="/" element={<TaskPage />} />
-        <Route path="/new" element={<Taskform />} />
-        <Route path="/edit/:id" element={<Taskform />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </TaskContextProvider>
+        <TaskContextProvider>
+          <Routes>
+            <Route path="/" element={<TaskPage />} />
+            <Route path="/new" element={<Taskform />} />
+            <Route path="/edit/:id" element={<Taskform />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TaskContextProvider>
       </div>
     </div>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router-dom";
-const Navbar = () => {
+
+const Navbar = (props) => {
   return (
-    <div className="bg-neutral-700 flex justify-between px-20 py-2">
+    
+    <div className=" sm:fixed top-0 left-0 right-0 flex place-content-between lg:px-16 sm:px-10 px-4 sm:py-5 py-4 sm:dark:bg-zinc-800 sm:shadow-sm sm:bg-zinc-100">
       <Link className="text-white font-bold" to="/">
         <h1>React MySQL</h1>
       </Link>
-      <ul className="flex gap-x-1">
-        <li>
-          <Link to="/" className="bg-slate-200 px-2 py-1">
-            Home
+      <ul className="flex gap-x-1 place-content-end">
+        {[
+          ["Home", "/"],
+          ["Create new", "/new"],
+        ].map(([title, url]) => (
+          <Link
+            key={title}
+            to={url}
+            className="sm:block hidden rounded-full px-3 py-1 dark:hover:bg-zinc-600  hover:bg-zinc-100 hover:text-teal-500"
+          >
+            {title}
           </Link>
-        </li>
-        <li>
-          <Link to="/new" className="bg-slate-200 px-2 py-1">
-            Create Task
-          </Link>
-        </li>
+        ))}
       </ul>
+      <img
+        onClick={props.toggleDarkMode}
+        className="self-left justify-self-end"
+        src={props.darkMode ? "./assets/sun.svg" : "./assets/moon.svg"} 
+      />
     </div>
   );
 };
